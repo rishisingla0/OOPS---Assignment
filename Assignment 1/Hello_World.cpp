eda765b2b47161d99540485574effb7b406bd7a2
Rishi Singla <rishisingla36@gmail.com>
2026-08-28 13:10:40 +0530

    Hello World

---
+++ Assignment 1/Hello_World.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    // New line
+    cout << "Hello\nWorld" << endl;
+
+    // Tab
+    cout << "Hello\tWorld" << endl;
+
+    // Alarm sound
+    cout << "Hello\a" << endl;
+
+    // Carriage return
+    cout << "Hello\rWorld" << endl;
+
+    // Using endl
+    cout << "Hello" << endl;
+    cout << "World";
+
+    return 0;
+}
